044816fbeab15961007f519294e53c0cdc3d9bbe
hchona <hchona1@gmail.com>
2019-09-03 18:20:40 -0400

    Here is a change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2019_Git_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
